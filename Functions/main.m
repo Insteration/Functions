64bc1ac2b37ce456199a5cf1cb0d777e70851033
Johansson <inste@icloud.com>
2019-01-05 14:54:33 +0200

    first commit

--- Functions/main.m
+++ Functions/main.m
@@ -8,10 +8,52 @@
 
 #import <Foundation/Foundation.h>
 
+void myFunction() {
+    NSLog(@"I'm myFunction!");
+}
+
+void myFunction2(NSInteger arg) {
+    NSLog(@"I'm myFunction2 and my argument is %i", arg);
+}
+
+void myFunction3(NSInteger a, NSInteger b) {
+    NSInteger c = a + b;
+    
+    NSLog(@"a + b = %li", c);
+}
+
+NSInteger myFunction4(NSInteger a, NSInteger b) {
+    NSInteger c = a + b;
+    
+    return c;
+}
+
+NSInteger myFunction5(NSInteger a, NSInteger b) {
+    NSInteger c;
+    if (b) {
+    NSInteger c = a / b;
+    } else {
+        NSLog(@"arg b = 0!");
+    }
+    return c;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //1
+        myFunction();
+        //2
+        myFunction2(15);
+        //3
+        myFunction3(13, 35);
+        //4
+        NSInteger d;
+        d = myFunction4(13, 35);
+        NSLog(@"%li", d);
+        //5
+        NSInteger e;
+        e = myFunction5(10, 0);
+        NSLog(@"%li", e);
     }
     return 0;
 }
